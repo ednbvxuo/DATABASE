790619cd0b4ef8f7b98a5262f86c98c312b73786
ednbvxuo <143352300+ednbvxuo@users.noreply.github.com>
2024-11-11 12:53:42 +0200

    Update DB Project.sql

--- DB Project.sql
+++ DB Project.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE Telecom_Team_4
 use DATABASE Telecom_Team_4
+create procedure createAllTables AS
+Begin 
+Create table Customer_profile(
+nationalID INT PRIMARY KEY,first_name VARCHAR(50),last_name VARCHAR(50),email VARCHAR(50),address VARCHAR(50),date_of_birth DATE)
